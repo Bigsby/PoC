962e402aaa5a413cd9ae01c6b3d6f201226ebdf4
Bigsby <bigsby@live.com>
2016-11-01 08:59:41 +0000

    Add angular component routing test

---
+++ Web/SiteEnginig/SiteEnginig/scripts/app.js
@@ -0,0 +1,30 @@
+﻿var appName = "mbApp";
+
+(function () {
+    "user strict";
+
+    var apiUrl = "../data/";
+
+    requirejs.config({
+        paths: {
+            "angular": "lib/angular.min",
+            "angularRoute": "lib/angular_1_router",
+            "text": "lib/require-text",
+            "json": "lib/require-json",
+            "api": apiUrl
+        }
+    });
+
+    requirejs(["json!api/shell.json", "angular", "angularRoute"], function (shell) {
+        var module = angular.module(appName, ["ngComponentRouter"]);
+        module.value("$routerRootComponent", "mbApp");
+
+        module.component("mbApp", {
+            templateUrl: "/templates/app.html",
+            $routeConig: [
+                { path: "/home", component: "home", "name": "Home" },
+
+            ]
+        });
+    });
+})();
