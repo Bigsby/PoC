3e0d0729eae323091500c31c75f76940895d55e9
Bigsby <bigsby@live.com>
2016-11-02 01:49:06 +0000

    Test parameterized urls

--- Web/AngularComponenting/AngularComponenting/scripts/app.js
+++ Web/AngularComponenting/AngularComponenting/scripts/app.js
@@ -16,25 +16,62 @@
     requirejs(["angular"], function () {
 
         requirejs(["json!api/structure/shell.json", "angularRouter"], function (shell) {
-            
+
             var app = angular.module(appName, ["ngComponentRouter"]);
             app.value("$routerRootComponent", "appContainer");
-            
+
             var routes = [];
 
+            shell.sections.push({
+                id: "home",
+                name: "Home"
+            });
+
+            shell.sections.push({
+                id: "appHeader",
+                name: "Header",
+                skipRoute: true
+            });
+
+            shell.sections.push({
+                id: "appFooter",
+                name: "Footer",
+                skipRoute: true
+            });
+
             angular.forEach(shell.sections, function (section) {
+
                 app.component(section.id, {
                     templateUrl: "data/templates/" + section.id + ".html",
                     controller: function () {
-                        this.message = "This is " + section.name + " message"
+                        var vm = this;
+                        vm.message = "This is " + section.name + " message";
+                        vm.$routerOnActivate = function (next, previous) {
+                            if (next.params.id)
+                                vm.message = "Your id is: " + next.params.id;
+                        };
                     }
                 });
 
-                routes.push({
-                    path: "/" + section.id,
-                    component: section.id,
-                    name: section.name
-                });
+                if (!section.skipRoute) {
+                    routes.push({
+                        path: "/" + (section.id == "home" ? "" : section.id),
+                        component: section.id,
+                        name: section.name
+                    });
+                    if (section.id != "home")
+                        routes.push({
+                            path: "/" + section.id + "/:id",
+                            component: section.id,
+                            name: section.name + "Details"
+                        });
+                }
+
+            });
+
+            routes.push({
+                path: "/**",
+                redirectTo: ["Home", ""]
             });
 
             app.component("appContainer", {
