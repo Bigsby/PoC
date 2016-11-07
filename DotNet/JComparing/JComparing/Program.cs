8031afc9ff6c69b7d51cad7e700524a9dfc58ea0
Bigsby <bigsby@live.com>
2016-11-07 08:36:57 +0000

    JComparing

---
+++ DotNet/JComparing/JComparing/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace JComparing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var one = JValue.FromObject("one");
+            var two = JValue.FromObject("one");
+
+            Console.WriteLine(1.CompareTo(2));
+            Console.WriteLine(1.CompareTo(1));
+            Console.WriteLine(1.CompareTo(0));
+            Console.WriteLine(one == two);
+            Console.ReadLine();
+        }
+    }
+}
