75b4674dc11ab74306a7cd0a9f89091398596ab3
Bigsby <bigsby@live.com>
2016-11-21 11:06:50 +0000

    Several tests

---
+++ DotNet/HTMLCrawling/HTMLCrawling/Program.cs
@@ -0,0 +1,57 @@
+﻿using HtmlAgilityPack;
+using System.Net.Http;
+using System.Web;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using static System.Console;
+
+namespace HTMLCrawling
+{
+    class Program
+    {
+        private const string _baseUrl = "https://channel9.msdn.com/";
+        private const string _pagesUrl = _baseUrl + "Events/Connect/2016?sort=status&direction=desc&page=";
+        private const int pageCount = 11;
+        private const string _resultOutput = @"C:\Git\Bigsby\PoC\DotNet\HTMLCrawling\HTMLCrawling\Connect2016Sessions.json";
+
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var result = new List<Session>();
+            for (int page = 1; page < pageCount + 1; page++)
+            {
+                WriteLine($"Processing page {page}");
+                var response = client.GetStringAsync(_pagesUrl + page).Result;
+                var html = new HtmlDocument();
+                html.LoadHtml(response);
+                var navigator = html.CreateNavigator();
+
+                foreach (HtmlNodeNavigator article in navigator.Select("/html/body/main/section/div/article"))
+                    result.Add(new Session
+                    {
+                        Title = HttpUtility.HtmlDecode(article.SelectSingleNode("header/h4/a").Value),
+                        Url = _baseUrl + article.SelectSingleNode("a").GetAttribute("href", null),
+                        Duration = TimeSpan.Parse("00:" + article.SelectSingleNode("a/time").Value)
+                    });
+            }
+
+            WriteLine("Serializing");
+            var json = JsonConvert.SerializeObject(result);
+            WriteLine("Saving to file");
+            File.WriteAllText(_resultOutput, json);
+            WriteLine("Done!!!");
+            ReadLine();
+        }
+    }
+
+    public class Session
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public TimeSpan Duration { get; set; }
+        public int Relevance { get; set; }
+    }
+}
---
+++ Web/Materializing/EggHeadIntroduction/scripts/app.js
@@ -0,0 +1,85 @@
+﻿(function () {
+    "use strict";
+    var appName = "testApp";
+    var mainComponentName = "appContainer";
+
+    requirejs.config({
+        "paths": {
+            "text": "lib/require-text",
+            "json": "lib/require-json"
+        },
+        onerror: function (error) {
+            alert("error");
+        }
+    });
+
+    function loadCss(style) {
+        var link = document.createElement("link");
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.href = "styles/" + style.path;
+        document.getElementsByTagName("head")[0].appendChild(link);
+        console.log("Loade: " + link.href);
+    }
+
+    requirejs(["json!../shell.json"], function (shell) {
+        var sortedStyles = shell.styles.sort(function (a, b) { return a.order - b.order; });
+
+        for (var styleIndex = 0; styleIndex < sortedStyles.length; styleIndex++) {
+            loadCss(sortedStyles[styleIndex]);
+        }
+
+        requirejs([shell.scripts.path], function () {
+            console.log("Loaded: " + shell.scripts.path);
+            requirejs(shell.scripts.dependants, function () {
+                console.log("Loaded: " + shell.scripts.dependants.join());
+                var module = angular.module(appName, ["ngComponentRouter", "ngSanitize", "ngMaterial"]);
+                module.component(mainComponentName, {
+                    templateUrl: "templates/app.html",
+                    $routeConfig: [
+                        {
+                            path: "/first",
+                            component: "first",
+                            name: "First"
+                        },
+                        {
+                            path: "/second",
+                            component: "second",
+                            name: "Second"
+                        },
+                        {
+                            path: "/third",
+                            component: "third",
+                            name: "Third"
+                        },
+                        {
+                            path: "/fourth",
+                            component: "fourth",
+                            name: "Fourth"
+                        }
+                    ]
+                });
+
+                module.component("first", {
+                    templateUrl: "templates/first.html"
+                });
+
+                module.component("second", {
+                    templateUrl: "templates/second.html"
+                });
+
+                module.component("third", {
+                    templateUrl: "templates/third.html"
+                });
+
+                module.component("fourth", {
+                    templateUrl: "templates/fourth.html"
+                });
+
+                module.value("$routerRootComponent", mainComponentName);
+
+                angular.bootstrap(document, [appName]);
+            });
+        });
+    });
+})();
