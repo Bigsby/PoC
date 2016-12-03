8fe2bac7f9968fce9ad71967accf6cb55011235c
Bigsby <bigsby@live.com>
2016-12-03 15:47:51 +0000

    WPF bind string intrapolation

---
+++ Other/WPF.StringIntrapolation/WPF.StringIntrapolation/IntrapolationConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
+
+namespace WPF.StringIntrapolation
+{
+    public class IntrapolationConverter : IValueConverter
+    {
+        private static Regex _replaceRegex = new Regex(@"(\{[^\}]+\})", RegexOptions.Compiled);
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var format = parameter as string;
+            if (string.IsNullOrEmpty(format))
+                return string.Empty;
+
+            return _replaceRegex.Replace(format, match => Process(value, match));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string Process(object dataContext, Match match)
+        {
+            var propertyPath = match.Groups[1].Value.Trim('{', '}');
+
+            var value = GetValue(dataContext, propertyPath);
+            
+            return null == value ? string.Empty : value.ToString();
+        }
+
+        private object GetValue(object dataContext, string propertyPath)
+        {
+            try
+            {
+                var propertyNames = propertyPath.Split('.');
+                var currentValue = dataContext;
+                foreach (var property in propertyNames)
+                {
+                    if (currentValue == null)
+                        return null;
+
+                    currentValue = currentValue.GetType().GetProperty(property).GetValue(currentValue);
+                }
+
+                return currentValue;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Other/WPF.StringIntrapolation/WPF.StringIntrapolation/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WPF.StringIntrapolation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new Item
+            {
+                Name = "Parent",
+                Child = new Item
+                {
+                    Name = "Child"
+                }
+            };
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public Item Child { get; set; }
+    }
+}
