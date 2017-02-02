decd1f486641a19f22bd34637e058d201306518d
Bruno Bernardo <bigsby@live.com>
2017-02-02 16:02:20 +0000

    Add email testing

---
+++ Web/GoDaddyEmailing/GoDaddyEmailing/Controllers/EmailController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Mail;
+using System.Web.Http;
+
+namespace GoDaddyEmailing.Controllers
+{
+    public class EmailController : ApiController
+    {
+        [HttpPost]
+        public string Send([FromBody]Message message)
+        {
+            var m = new MailMessage();
+
+            m.Subject = message.Subject;
+            m.Body = message.Content;
+
+            m.To.Add(new MailAddress(message.To));
+            var client = new SmtpClient();
+            try
+            {
+                client.Send(m);
+                return "Sent!";
+            }
+            catch (Exception ex)
+            {
+                return GetMessage(ex);
+            }
+        }
+
+        private string GetMessage(Exception ex)
+        {
+            var current = ex;
+
+            while (null != current.InnerException)
+                current = current.InnerException;
+
+            return current.Message;
+        }
+    }
+
+    public class Message {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Web/GoDaddyEmailing/GoDaddyEmailing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoDaddyEmailing.Global" Language="C#" %>
