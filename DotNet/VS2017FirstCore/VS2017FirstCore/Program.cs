ac6f3f44486c483fb989bad0e1ee65cfd90d272f
Bigsby <bigsby@live.com>
2017-03-08 01:19:49 +0000

    DotNet Core test

---
+++ DotNet/VS2017FirstCore/VS2017FirstCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VS2017FirstCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
