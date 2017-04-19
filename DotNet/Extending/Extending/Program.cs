a5045d4146308986f5c18bfcc283fc43d7894b02
Bruno Bernardo <bigsby@live.com>
2017-04-19 17:47:42 +0100

    Extension method test

---
+++ DotNet/Extending/Extending/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Extending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello".PlusDot());
+            try
+            {
+                Console.Write(((string)null).PlusDot());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static string PlusDot(this string value)
+        {
+            if (null == value)
+                throw new ArgumentNullException(nameof(value));
+
+            return value + ".";
+        }
+    }
+}
