fb95afe473c265d55c4f74b20c7917e3450d518f
Bruno Bernardo <bigsby@live.com>
2017-05-16 17:34:55 +0100

    Local settings

---
+++ DotNet/LocalSettings/TransformSettings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using static System.Console;
+
+namespace TransformSettings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string key in ConfigurationManager.AppSettings.Keys)
+            {
+                WriteLine($"Configuration for \"{key}\" is \"{ConfigurationManager.AppSettings[key]}\"");
+            }
+
+            ReadLine();
+        }
+    }
+}
---
+++ DotNet/Serializing/Serializing/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Serializing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var item = new Item
+            {
+                Name = "This is the item",
+                Value = Value.Two
+            };
+
+            var json1 = JsonConvert.SerializeObject(item);
+
+            var deserializded1 = Deserialize<Item>(json1);
+
+            JsonConvert.DefaultSettings = (() =>
+            {
+                var settings = new JsonSerializerSettings();
+                settings.Converters.Add(new StringEnumConverter());
+                return settings;
+            });
+
+            var json2 = JsonConvert.SerializeObject(item);
+
+            var deserializded2 = Deserialize<Item>(json2);
+
+            var stopt = "here";
+        }
+
+        static T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public Value Value { get; set; }
+    }
+
+    public enum Value
+    {
+        One, Two
+    }
+}
