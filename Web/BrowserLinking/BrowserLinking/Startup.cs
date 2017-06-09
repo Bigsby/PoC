e94412fc794f512c673e5d3201681528f32b5c0d
Bruno Bernardo <bigsby@live.com>
2017-06-09 15:24:13 +0100

    Testing browser link

---
+++ Web/BrowserLinking/BrowserLinking/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Microsoft.AspNetCore.Builder;
+
+[assembly: OwinStartup(typeof(BrowserLinking.Startup))]
+
+namespace BrowserLinking
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            BrowserLinkLoaderExtensions.UseBrowserLink(app);
+            var apiConfig = new HttpConfiguration();
+            apiConfig.Routes.MapHttpRoute("default", "api/{controller}/{action}");
+            app.UseWebApi(apiConfig);
+
+            var fileSystem = new PhysicalFileSystem("wwwroot");
+            var staticFilesConfig = new FileServerOptions
+            {
+                FileSystem = fileSystem
+            };
+            staticFilesConfig.DefaultFilesOptions.DefaultFileNames = new[]
+            {
+                "index.html"
+            };
+            app.UseFileServer(staticFilesConfig);
+
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("OWIN here!");
+            });
+        }
+    }
+}
