4781a1d7243483b24591f14560aa6a3fb3ad2cee
Bigsby <bigsby@live.com>
2017-06-10 13:07:15 +0100

    Test Hashing

---
+++ DotNet/Hashing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Console;
+
+namespace Hashing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Passwprd:");
+            var pass = ReadLine();
+            WriteLine("Hash:");
+            WriteLine(HashPassword(pass));
+            WriteLine("Hash again:");
+            WriteLine(HashPassword(pass));
+            ReadLine();
+        }
+
+        public static string HashPassword(string password)
+        {
+            var sha1 = new SHA1CryptoServiceProvider();
+            var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
