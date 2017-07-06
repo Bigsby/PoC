ac550d09570b8b0c3fb8e32eadbdb75116217140
Bruno Bernardo <bigsby@live.com>
2017-07-06 15:52:54 +0100

    Generic OData

---
+++ Web/ODading/DynamicCollection/Controller/DataController.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Hosting;
+using System.Web.OData;
+
+namespace DynamicCollection.Controller
+{
+    public class DataController : ODataController
+    {
+        public IQueryable<Item> Get()
+        {
+            return DataProvider.GetItems(GetCollection()).AsQueryable();
+        }
+
+        public Item Get([FromODataUri]string key)
+        {
+            return DataProvider.GetItem(GetCollection(), key);
+        }
+
+        private string GetCollection()
+        {
+            return DynamicPathHandler.GetCollection((string)RequestContext.RouteData.Values["odataPath"]);
+        }
+    }
+
+    //Requests
+    //http://localhost:51270/data/one
+    //http://localhost:51270/data/one?$filter=contains(name,%27w%27)
+    //http://localhost:51270/data/one?$orderby=value
+    //http://localhost:51270/data/one?$orderby=value%20desc
+    //http://localhost:51270/data/one?$filter=value%20gt%20cast(3,%27Edm.Int64%27)
+
+    internal static class DataProvider
+    {
+        public static IEnumerable<Item> GetItems(string collection)
+        {
+            var folderPath = HostingEnvironment.MapPath($"~/App_Data/{collection}");
+            if (!Directory.Exists(folderPath))
+                yield break;
+
+            foreach (var filePath in Directory.GetFiles(folderPath))
+            {
+                var fileContent = File.ReadAllText(filePath);
+                yield return new Item {
+                    id = Path.GetFileNameWithoutExtension(filePath),
+                    InnerObject = ParseContent(fileContent)
+                };
+            }   
+        }
+
+        private static JToken ParseContent(string content)
+        {
+            var result = JToken.Parse(content);
+            foreach (var property in result)
+            {
+                //property.
+            }
+            return result;
+        }
+
+        public static Item GetItem(string collection, string id)
+        {
+            var filePath = HostingEnvironment.MapPath($"~/App_Data/{collection}/{id}.json");
+            if (!File.Exists(filePath))
+                return null;
+            var fileContent = File.ReadAllText(filePath);
+            return new Item {
+                id = id,
+                InnerObject = JToken.Parse(fileContent)
+            };
+        }
+    }
+}
---
+++ Web/ODading/DynamicCollection/Startup.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Web.OData.Routing;
+using System.Web.OData.Extensions;
+using System.Collections.Generic;
+using System;
+using Microsoft.OData.Edm;
+using Newtonsoft.Json.Linq;
+using System.Web.OData.Builder;
+using System.Web.OData.Routing.Template;
+using System.Web.OData.Routing.Conventions;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+[assembly: OwinStartup(typeof(DynamicCollection.Startup))]
+
+namespace DynamicCollection
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();            
+            config.Routes.MapHttpRoute("api", "api/{controller}/{action}");
+            ConfigureOdata(config);
+            app.UseWebApi(config);
+        }
+
+        static void ConfigureOdata(HttpConfiguration config)
+        {
+            var routePrefix = "data";
+            var routeName = "odata";
+
+            config.MapODataServiceRoute(routeName, routePrefix, Item.GetModel(), new DynamicPathHandler(), ODataRoutingConventions.CreateDefault());
+            config.Count().Filter().Select().OrderBy();
+            config.AddODataQueryFilter();
+        }
+    }
+
+    public class DynamicPathHandler : DefaultODataPathHandler
+    {
+        private static Regex _collectionRegex = new Regex("^([^/(\\?%]+)", RegexOptions.Compiled);
+
+        public override ODataPath Parse(string serviceRoot, string odataPath, IServiceProvider requestContainer)
+        {
+            return base.Parse(serviceRoot, _collectionRegex.Replace(odataPath, "data"), requestContainer);
+        }
+
+        public override ODataPathTemplate ParseTemplate(string odataPathTemplate, IServiceProvider requestContainer)
+        {
+            return base.ParseTemplate(odataPathTemplate, requestContainer);
+        }
+
+        internal static string GetCollection(string path)
+        {
+            return _collectionRegex.Match(path).Groups[1].Value;
+        }
+    }
+
+
+
+    public class Item
+    {
+        public string id { get; set; }
+
+        public JToken InnerObject { get; set; }
+
+        public IDictionary<string, object> DynamicProperties
+        {
+            get
+            {
+                return InnerObject == null ? new Dictionary<string, object>() : InnerObject.ToObject<Dictionary<string, object>>();
+            }
+        }
+
+        internal static IEdmModel GetModel()
+        {
+            var builder = new ODataModelBuilder();
+            var itemType = builder.EntityType<Item>();
+            itemType.HasKey(i => i.id);
+            itemType.HasDynamicProperties(i => i.DynamicProperties);
+            builder.EntitySet<Item>("data");
+
+            return builder.GetEdmModel();
+        }
+    }
+}
