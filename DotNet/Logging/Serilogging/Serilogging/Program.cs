ffdb335fa4b82ac98fbd9a8c82ef240da55b854b
Bruno Bernardo <bigsby@live.com>
2017-10-17 16:37:53 +0100

    Logging

---
+++ DotNet/Logging/Serilogging/Serilogging/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serilogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
