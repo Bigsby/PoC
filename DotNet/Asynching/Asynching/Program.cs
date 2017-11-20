570526b081e280604922114398206175381138d6
Bruno Bernardo <bigsby@live.com>
2017-11-20 12:05:56 +0000

    Multiple tests

---
+++ DotNet/Asynching/Asynching/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Asynching
+{
+    class Program
+    {
+        private static TaskCompletionSource<bool> _tcs;
+        static void Main(string[] args)
+        {
+            var date = DateTime.Now;
+            Console.WriteLine($"{date:s}");
+            DoAsyncStuff().Wait();
+            Console.WriteLine("Done waiting");
+        }
+
+        static async Task DoAsyncStuff()
+        {
+            _tcs = new TaskCompletionSource<bool>();
+
+            var doOne = DoOne();
+            var timeout = Task.Delay(2000);
+
+            var allTasks = Task.WhenAll(doOne, _tcs.Task);
+
+            if (allTasks != await Task.WhenAny(allTasks, timeout))
+            {
+                Console.WriteLine("Timeout finished first!");
+            }
+            else
+            {
+                Console.WriteLine("All finished first!");
+            }
+
+            Console.WriteLine("Done all!");
+            Console.ReadLine();
+        }
+
+        static async Task DoOne()
+        {
+            Console.WriteLine("Started one...");
+            await Task.Delay(1000);
+            Console.WriteLine("Done one!");
+            await Task.Delay(500);
+            Console.WriteLine("Setting flag...");
+            _tcs.TrySetResult(true);
+        }
+    }
+}
---
+++ DotNet/EventSubscription/EventSubscription/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace EventSubscription
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var publisher = new EventPublisher();
+            var subscriber = new EventSubscriber(publisher);
+
+            var isSubscribed = IsEventSubscribed(publisher, nameof(EventPublisher.TheEvent), subscriber);
+
+            var isActionSubscribed = IsEventSubscribed(publisher, p => p.TheEvent += null, subscriber);
+            var stop = "here";
+        }
+
+        static bool IsEventSubscribed<TPublisher>(TPublisher publisher, string eventName, object subscriber)
+        {
+            if (null == publisher || null == subscriber)
+                return false;
+
+            var eventField = typeof(TPublisher).GetField(eventName, BindingFlags.Instance | BindingFlags.NonPublic);
+
+            if (null == eventField)
+                return false;
+
+            var eventHandler = eventField.GetValue(publisher);
+
+            if (eventHandler is MulticastDelegate multicastDelegate)
+                return multicastDelegate.GetInvocationList().Any(s => 
+                    s.Target == subscriber 
+                    || s.Method.DeclaringType == subscriber.GetType() 
+                    || s.Method.DeclaringType?.DeclaringType == subscriber.GetType());
+            
+            return false;
+        }
+
+        static bool IsEventSubscribed<TPublisher>(TPublisher publisher, Action<TPublisher> eventExpression, object subscriber)
+        {
+            if (null == publisher || null == subscriber || null == eventExpression)
+                return false;
+
+            var body = eventExpression.GetMethodInfo()?.GetMethodBody();
+            if (null == body)
+                return false;
+
+
+            if (null == publisher || null == subscriber)
+                return false;
+
+
+            //var eventField = typeof(TPublisher).GetField(eventName, BindingFlags.Instance | BindingFlags.NonPublic);
+
+            //if (null == eventField)
+            //    return false;
+
+            //var eventHandler = eventField.GetValue(publisher);
+
+            //if (eventHandler is MulticastDelegate multicastDelegate)
+            //    return multicastDelegate.GetInvocationList().Any(s =>
+            //        s.Target == subscriber
+            //        || s.Method.DeclaringType == subscriber.GetType()
+            //        || s.Method.DeclaringType?.DeclaringType == subscriber.GetType());
+
+            return false;
+        }
+    }
+
+    public class EventPublisher
+    {
+        public event EventHandler TheEvent;
+    }
+
+    public class EventSubscriber
+    {
+        public EventSubscriber(EventPublisher publisher)
+        {
+            publisher.TheEvent += (s, e) => { };
+        }
+
+        private void Publisher_TheEvent(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
