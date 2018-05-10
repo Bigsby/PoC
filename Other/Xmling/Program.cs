89ef481244117b55dc04c8c1ee9588f233cac9ea
Bruno Bernardo <bigsby@live.com>
2018-05-10 12:04:04 +0100

    Xml new line test

---
+++ Other/Xmling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml;
+using static System.Console;
+
+namespace Xmling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new XmlDocument();
+            doc.Load("file.xml");
+            var elements = doc.GetElementsByTagName("element");
+            foreach (XmlNode node in elements)
+            {
+                WriteLine(node.InnerText);
+            }
+
+            var attrs = doc.GetElementsByTagName("attr");
+            foreach (XmlNode attr in attrs)
+            {
+                WriteLine(attr.Attributes["value"].Value);
+            }
+            ReadLine();
+            WriteLine("Done!");
+        }
+    }
+}
