8776e4786afb223e82c9f56849f83073f3a60799
Bruno Bernardo <bigsby@live.com>
2018-06-13 21:53:44 +0100

    Loads of tests

---
+++ Other/ConsoleCapture/fs/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+// [<EntryPoint>]
+// let main =
+while true  do
+    let key = Console.ReadKey true
+    match key.Key with
+    | ConsoleKey.Q -> Environment.Exit 0
+    | _ -> printfn "%c" key.KeyChar
+    
+    //0
